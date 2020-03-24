c950bc621b97e467bdcdda469b4160f7f4e415cb
Tanay Singhania <singhaniatanay18@gmail.com>
2020-03-24 17:26:21 +0530

    first

---
+++ app/src/main/java/tech/tanaysinghania/interviewapp/BaseActivity.java
@@ -0,0 +1,44 @@
+package tech.tanaysinghania.interviewapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ProgressBar;
+
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    public ProgressBar mProgressBar;
+
+    public void setProgressBar(int resId) {
+        mProgressBar = findViewById(resId);
+    }
+
+    public void showProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void hideKeyboard(View view) {
+        final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressBar();
+    }
+
+}
